7965c3b2303215e5a2a99ca43d4d9bf18ae85f8a
SergeyVolkoff <vol7745@yandex.ru>
2023-09-20 09:24:22 -0400

    bm

--- BM10.py
+++ BM10.py
@@ -33,14 +33,14 @@ class BM10():
                 temp = yaml.safe_load(f2)
                 for t in temp:
                     device = dict(t)
-
             self.ssh = ConnectHandler(**device)
             self.ip = host
             self.name = username
             self.passwd = password
-            self.promo = " -w 4"
+            self.promo_ping = " -w 4"
+            self.promt_tracert = '-m 3'
             self.word_ping = "ping "
-            self.command_ping = self.word_ping+self.promo
+            self.command_ping = self.word_ping+self.promo_ping
         except(NetmikoAuthenticationException,NetmikoTimeoutException) as error:
             print("*" * 5, "Error connection to:", device['host'], "*" * 5)
 
@@ -52,7 +52,7 @@ class BM10():
         """
 
         if log:
-            console.print(f"Connect to {device['host']}...", style="warning")
+            console.print(f"Try connect to {device['host']}...", style="warning")
         try:
             with ConnectHandler(**device) as ssh:
                 console.print(device['host'], "connected!", style='success')
@@ -60,18 +60,124 @@ class BM10():
             console.print("*" * 5, "Error connection to:", device['host'], "*" * 5, style='fail')
 
 
-    def send_sh_command(self, device, command):
+    def send_command(self, device, command):
 
         """ФУНКЦИЯ отправки простой команды в уст-во по ssh, без импорта"""
 
-        self.check_connection(device)         # вызов функции проверки соединения с роутером
+        #self.check_connection(device)         # вызов функции проверки соединения с роутером
         temp = self.ssh.send_command(command)
         result = temp
         return result
+
+
+    def ping_inet(self, device):
+
+        """ФУНКЦИЯ для простого пинга,  запросит адрес назначения, формат команды прописан в инит.
+        без импорта."""
+
+        self.check_connection(device)
+        ip_for_ping = "8.8.8.8"
+        command_ping = (self.word_ping + ip_for_ping + self.promo_ip)
+        print(command_ping)
+        output = self.ssh.send_command(command_ping)
+        if "round-trip min/avg/max" in output:
+            output = re.search(r'round-trip min/avg/max = (\S+ ..)', output).group()
+            result = ["IP", ip_for_ping, "destination available :", output]
+            result = ' '.join(result)
+        else:
+            result = ["Ip", ip_for_ping, "out of destination"]
+            result = ' '.join(result)
+        return result
+        print(output)
+
+
+    def ping_ip(self, device,ip_for_ping):
+
+        """ФУНКЦИЯ для простого пинга,  запросит адрес назначения, формат команды прописан в инит.
+        без импорта."""
+
+        self.check_connection(device)
+        command_ping = (self.word_ping + ip_for_ping + self.promo_ip)
+        print(command_ping)
+        output = self.ssh.send_command(command_ping)
+        if "round-trip min/avg/max" in output:
+            output = re.search(r'round-trip min/avg/max = (\S+ ..)', output).group()
+            result = ["IP", ip_for_ping, "destination available :", output]
+            result = ' '.join(result)
+        else:
+            result = ["Ip", ip_for_ping, "out of destination"]
+            result = ' '.join(result)
+        return result
+        print(output)
+
+
+    def tracert_inet(self,device):
+
+        """ФУНКЦИЯ для  tracert 8.8.8.8 for pppoe peer!!!"""
+
+        ip_tracert = '8.8.8.8'
+        comand_tracert = f'traceroute {ip_tracert} {self.promt_tracert}'
+        output_tracert = self.ssh.send_command(comand_tracert)
+        if "ms" in output_tracert:
+            temp = self.send_command(device,'ip a')
+            if "peer" in temp:
+                output1 = re.search(r'\s+inet (?P<ip_int>\d+.\d+.\d+.\d+) peer (?P<ip_peer>\d+.\d+.\d+.\d+).{0,}pppoe-wan',
+                                temp)
+                ip_peer = output1.group('ip_peer')
+                result = f'### Tracert passes through server-peer, {ip_peer}! ###\n {output_tracert}'
+            else:
+                result = f'### Tracert passes! ###\n {output_tracert}'
+        else:
+            result = f'Tracert does not pass through {output_tracert}'
+
+        return result
+        print(output)
+
+
+    def tracert_ip(self,device,ip_tracert):
+
+        """ФУНКЦИЯ для  tracert ip"""
+
+        #self.check_connection(device)
+        comand_tracert = f'traceroute {ip_tracert} {self.promt_tracert}'
+        output_tracert = self.ssh.send_command(comand_tracert)
+        if "ms" in output_tracert:
+            temp = self.send_command(device,'ip a')
+            if "peer" in temp:
+                output1 = re.search(r'\s+inet (?P<ip_int>\d+.\d+.\d+.\d+) peer (?P<ip_peer>\d+.\d+.\d+.\d+).{0,}pppoe-wan',
+                                temp)
+                ip_peer = output1.group('ip_peer')
+                result = f'### Tracert passes through server-peer, {ip_peer}! ###\n {output_tracert}'
+            else:
+                result = f'### Tracert passes! ###\n {output_tracert}'
+        else:
+            result = f'Tracert does not pass through {output_tracert}'
+
+        return result
+        print(output)
+
+    def reset_conf(self,device, comm_reset_conf):
+        
+        """ ФУНКЦИЯ сброса конфига на заводской, с ребутом устр-ва.
+        без импорта
+        """
+
+        self.check_connection(device)
+        output = self.ssh.send_command("uci show system.@system[0].hostname")
+        print(output)
+        if "DUT" in output:
+            result_reset=self.ssh.send_config_set(self.commands_to_reset_conf)
+            return result_reset
+        
+
 if __name__ == "__main__":
     with open("src/BM10_LTE.yaml")as f:
-         temp = yaml.safe_load(f)
-         for t in temp:
+        temp = yaml.safe_load(f)
+        for t in temp:
             device = dict(t)
             r1 = BM10(**device)
-            print(r1.send_sh_command(device, "uci show"))
+            #print(r1.send_command(device, "uci show"))
+            print(r1.tracert_ip(device,ip_tracert='8.8.8.8'))
+
+
+
