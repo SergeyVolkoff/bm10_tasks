e0a0f5723da75c3d973f6b9699f3cebb5854e338
SergeyVolkoff <vol7745@yandex.ru>
2023-09-22 07:06:15 -0400

    bm

--- Cfg_bm10.py
+++ Cfg_bm10.py
@@ -24,55 +24,141 @@ from rich.theme import Theme
 
 
 
-class Cfg_BM10(Base_bm10):
-    
+class Cfg_templ_bm10(Base_bm10):
 
+    def cfg_template(self,device,commands_template):
+
+        """ФУНКЦИЯ настройки базового конфига"""
+
+        self.check_connection(device)
+        result = {}
+        for command in commands_template:
+            output = self.ssh.send_command(command, expect_string="", read_timeout=1)
+            if "mwan3" in command:
+                    result_command = "wait, please"
+                    print(command, result_command)
+                    time.sleep(3)
+            if "commit" in command:
+                result_command = "wait, please"
+                print(command, result_command)
+                time.sleep(3)
+            if "" in output:
+                result_command = "command passed"
+                result[command]=output
+                print(command,result_command)
+            elif "Usage: uci [<options>] <command> [<arguments>]" in output:
+                result_command = "bad command"
+                print(command, result_command)
+                result[command] = result_command
+        return result
+
+class Cfg_bm10(Cfg_templ_bm10):
+
+    
     def cfg_pass (self,device, commands, log=True):
 
-            """ ФУНКЦИЯ изменения пароля, надо поменять так,
-            чтоб можно было вводить короткий пароль без сбоя и тащить пароль со стороны, а не из кода.
-            без импорта"""
-            
-            # if log:
-            #     self.console.print(f"Connect to {device['host']}...",style="success") # style переменная rich, назначает цвет выводу
-            # result = ''
-            # try:
-            #     with ConnectHandler(**device) as ssh:
-            new_pass = input("Input new pass: ")
-            prompt = self.ssh.find_prompt()
-            output = self.ssh.send_command(commands, expect_string="New password:", read_timeout=2)
+        """ ФУНКЦИЯ изменения пароля, надо поменять так,
+        чтоб можно было вводить короткий пароль без сбоя и тащить пароль со стороны, а не из кода.
+        без импорта"""
+        
+        self.check_connection(device)
+        new_pass = input("Input new pass: ")
+        prompt = self.ssh.find_prompt()
+        output = self.ssh.send_command(commands, expect_string="New password:", read_timeout=2)
+        print(output, "****")
+        if "New" in output:
+            output = self.ssh.send_command_timing(new_pass, read_timeout=1)
             print(output, "****")
-            if "New" in output:
+            if "Bad password" not in output:
+                pass
+            else:
                 output = self.ssh.send_command_timing(new_pass, read_timeout=1)
-                print(output, "****")
-                if "Bad password" not in output:
-                    pass
-                else:
-                    output = self.ssh.send_command_timing(new_pass, read_timeout=1)
-                    self.console.print(output,style="success")
-                if "Re-enter new password:" in output:
-                    output = self.ssh.send_command_timing(new_pass, read_timeout=1)
-                    self.console.print(output,style="warning")
-                    while True:
-                        if "root@" not in output:
-                            output = self.ssh.read_until_pattern(f'{prompt}', read_timeout=0)
-                            print("Wait, the password will change now")
-                            self.ssh.write_channel(" ")
-                        elif "root@" in output:
-                            self.console.print("New pass OK",style="success")
-                            break
-                return output
-            # except (NetmikoAuthenticationException, NetmikoTimeoutException) as error:
-            #     print("*" * 5, "Error connection to:", device['host'], "*" * 5)
+                self.console.print(output,style="success")
+            if "Re-enter new password:" in output:
+                output = self.ssh.send_command_timing(new_pass, read_timeout=1)
+                self.console.print(output,style="warning")
+                while True:
+                    if "root@" not in output:
+                        output = self.ssh.read_until_pattern(f'{prompt}', read_timeout=0)
+                        print("Wait, the password will change now")
+                        self.ssh.write_channel(" ")
+                    elif "root@" in output:
+                        self.console.print("New pass OK",style="success")
+                        break
+            return output
+            
+    def cfg_base(self,device, commands_template):
+        
+        """ФУНКЦИЯ настройки базового конфига"""
+
+        Cfg_templ_bm10.cfg_template(self,device,commands_template)
+        return
+        
+        # self.check_connection(device)
+        # result = {}
+        # for command in self.commands_base_cfg:
+        #     output = self.ssh.send_command(command, expect_string="", read_timeout=1)
+        #     if "mwan3" in command:
+        #             result_command = "wait, please"
+        #             print(command, result_command)
+        #             time.sleep(3)
+        #     if "commit" in command:
+        #         result_command = "wait, please"
+        #         print(command, result_command)
+        #         time.sleep(3)
+        #     if "" in output:
+        #         result_command = "command passed"
+        #         result[command]=output
+        #         print(command,result_command)
+        #     elif "Usage: uci [<options>] <command> [<arguments>]" in output:
+        #         result_command = "bad command"
+        #         print(command, result_command)
+        #         result[command] = result_command
+        # return result
+    
+        
+    def cfg_base_802(self, device, commands_template):
+
+        """ФУНКЦИЯ настройки stp- конфига"""
+
+        Cfg_templ_bm10.cfg_template(self,device,commands_template)
+        return
 
 
+        # self.check_connection(device)
+        # result = {}
+        # for command in self.commands_802_1d_cfg:
+        #     output = self.ssh.send_command(command, expect_string="", read_timeout=1)
+        #     #time.sleep(3) # только для 802
+        #     if "mwan3" in command:
+        #             result_command = "wait, please"
+        #             print(command, result_command)
+        #             time.sleep(3)
+        #     if "commit" in command:
+        #         result_command = "wait, please"
+        #         print(command, result_command)
+        #         time.sleep(3)
+        #     if "" in output:
+        #         result_command = "command passed"
+        #         result[command]=output
+        #         print(command,result_command)
+        #     elif "Usage: uci [<options>] <command> [<arguments>]" in output:
+        #         result_command = "bad command"
+        #         print(command, result_command)
+        #         result[command] = result_command
+        # return result
 
+
+    
 
 if __name__ == "__main__":
     with open("src/BM10_LTE.yaml")as f:
         temp = yaml.safe_load(f)
         for t in temp:
             device = dict(t)
-            r1 = Cfg_BM10(**device)
-            print(r1.cfg_pass(device, commands='passwd'))
-            
+            r1 = Cfg_bm10(**device)
+            #print(r1.cfg_pass(device, commands='passwd'))
+            #print(r1.cfg_base(device,r1.commands_base_cfg))
+            #print (r1.cfg_base_802(device, r1.commands_802_1d_cfg))
+            #print(r1.cfg_base(device, commands_template=r1.commands_base_cfg))
+            print(r1.cfg_base_802(device, commands_template=r1.commands_802_1d_cfg))
