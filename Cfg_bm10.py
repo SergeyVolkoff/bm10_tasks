24e7170249238a9e334acdca0b1b4b3801cc2c1c
SergeyVolkoff <vol7745@yandex.ru>
2023-09-26 04:12:50 -0400

    bm

--- Cfg_bm10.py
+++ Cfg_bm10.py
@@ -95,6 +95,41 @@ class Cfg_bm10(Cfg_templ_bm10):
         Cfg_templ_bm10.cfg_template(self,device,commands_template)
         return
 
+
+    def cfg_pppoe_serv(self,device,commands_template):
+
+        """ 4-ФУНКЦИЯ настройки роутера как РРРоЕ-server на wan порту
+        Сервр льем первым!
+        эта ф-я передает донастривает рррое и имя на интерфейсе """
+        
+        Cfg_templ_bm10.cfg_template(self,device,commands_template)
+        return
+    
+
+    def cfg_ripv2(self, device,commands_template):
+
+        """ ФУНКЦИЯ настройки RIPv2 """
+
+        Cfg_templ_bm10.cfg_template(self,device,commands_template)
+        return
+    
+
+    def cfg_ripvng(self, device, commands_template): 
+        
+        """ ФУНКЦИЯ настройки RIPng """
+
+        Cfg_templ_bm10.cfg_template(self,device,commands_template)
+        return
+
+
+    def cfg_ospfv2(self, device,commands_template): 
+
+        """ ФУНКЦИЯ настройки OSPFv2 """
+
+        Cfg_templ_bm10.cfg_template(self,device,commands_template)
+        return
+
+
     def cfg_pass (self,device, commands, log=True):
 
         """ ФУНКЦИЯ изменения пароля, надо поменять так,
@@ -129,7 +164,7 @@ class Cfg_bm10(Cfg_templ_bm10):
 
     
         
-class SCP_cfg_ppoe():
+class SCP_cfg_ppoe(Cfg_templ_bm10):
     
     def __init__(self):
         
@@ -142,19 +177,43 @@ class SCP_cfg_ppoe():
         password='root',
         port=22
         )
-    def cfg_pppoe_serv1(self):
+    def cfg_pppoe_serv(self):
 
         """1-ФУНКЦИЯ настройки роутера как РРРоЕ-server на wan порту
         Сервр льем первым!
         эта ф-я передает файл pppoe в DUT, в файле лежат настройки сервера ррре"""
 
-        #client = SSHClient()
         # SCPCLient takes a paramiko transport as an argument
         scp = SCPClient(self.client.get_transport())
-        scp.put('src/file_cfg/pppoe', '/etc/config/')
+        scp.put('src/pppoe_cfg_file/pppoe', '/etc/config/')
         scp.close()
         self.client.close()
 
+    def cfg_pppoe_opt(self):
+
+        """2-ФУНКЦИЯ настройки роутера как РРРоЕ-server на wan порту
+        Сервр льем первым!
+        эта ф-я передает файл pppoe-server-options в DUT, в файле лежит require-chaр,echo-interval"""
+        
+        # SCPCLient takes a paramiko transport as an argument
+        scp = SCPClient(self.client.get_transport())
+        scp.put('src/pppoe_cfg_file/pppoe-server-options', '/etc/ppp/')
+        scp.close()
+        self.client.close()
+    
+    def cfg_pppoe_chap(self):
+
+        """3-ФУНКЦИЯ настройки роутера как РРРоЕ-server на wan порту
+        Сервр льем первым!
+        эта ф-я передает файл chap-secrets в DUT, в файле лежит login-pass"""
+        
+        # SCPCLient takes a paramiko transport as an argument
+        scp = SCPClient(self.client.get_transport())
+        scp.put('src/pppoe_cfg_file/chap-secrets', '/etc/ppp/')
+        scp.close()
+        self.client.close()
+
+        
 if __name__ == "__main__":
     with open("src/BM10_LTE.yaml")as f:
         temp = yaml.safe_load(f)
@@ -165,9 +224,18 @@ if __name__ == "__main__":
             #print(r1.cfg_pass(device, commands='passwd'))
             #print(r1.cfg_base(device,r1.commands_base_cfg))
             #print (r1.cfg_base_802(device, r1.commands_802_1d_cfg))
-            #print(r1.cfg_base(device, commands_template=r1.commands_base_cfg))
-            #print(r1.cfg_base_802(device, commands_template=r1.commands_802_1d_cfg))
-            #print(r1.cfg_vlan(device,commands_template=r1.commands_vlan_cfg))
-            #print(r1.cfg_WiFi_AP(device,commands_template=r1.commands_cfg_WiFi_AP))
+            print(r1.cfg_base(device, r1.commands_base_cfg))
+            #print(r1.cfg_base_802(device, r1.commands_802_1d_cfg))
+            #print(r1.cfg_vlan(device,r1.commands_vlan_cfg))
+            #print(r1.cfg_WiFi_AP(device,r1.commands_cfg_WiFi_AP))
             #print(r1.cfg_pppoe_client(device,r1.commands_pppoe_client_cfg))
-            print(r2.cfg_pppoe_serv1())
+            #print(r1.cfg_ripv2(device,r1.commands_cfg_ripv2))
+            #print(r1.cfg_ripvng(device,r1.commands_cfg_ripng))
+            print(r1.cfg_ospfv2(device,r1.commands_cfg_ospfv2))
+            
+
+
+            #print(r2.cfg_pppoe_serv())
+            #print(r2.cfg_pppoe_opt())
+            #print(r2.cfg_pppoe_chap())
+            print(r1.cfg_pppoe_serv(device,r1.commands_pppoe_server_cfg))
