38e8834348fbafd0c5cb6bd4b5fe8a0363762c66
SergeyVolkoff <vol7745@yandex.ru>
2023-10-05 15:00:34 -0400

    bm

--- src/main_module/ixiaapi/base_ixia.py
+++ src/main_module/ixiaapi/base_ixia.py
@@ -8,15 +8,15 @@ class Base_ixia():
     def __init__(self):
         self.ixiaApiServer = "127.0.0.1"
         self.ixiaApiPort = '11009'
-        self.ixiaChassis = "10.27.193.3"
-        self.connect = IxAPI(self.ixiaApiServer, self.ixiaApiPort)
+        self.ixiaChassis = "10.27.192.3"
+        
         
 
 
     def connect_ixia(self):
 
         """ Подключение к IXIA. Загрузка конфигурационного файла (данные по API серверу указану в IxiaApi.py, файл ниже)"""
-        
+        self.connect = IxAPI(self.ixiaApiServer, self.ixiaApiPort)
         self.connect.conn_srvr()
         self.connect.verif_sessions()
         file = "ixiaapi/cfgs_ixia/Cfg_access_trunk.ixncfg"
