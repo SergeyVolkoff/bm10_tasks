9b6b6af81baf6fe8bd5ca601dafc470dd99ad57b
SergeyVolkoff <vol7745@yandex.ru>
2023-10-17 14:30:43 -0400

    bm

--- src/main_module/gns3/base_gns3.py
+++ src/main_module/gns3/base_gns3.py
@@ -3,16 +3,17 @@ from gns3fy import Gns3Connector, Project, Node, Link
 from tabulate import tabulate
 
 class Base_gns():
+
+    
     def __init__(self):
 
         """ Определение коннектора(connector) и проекта (lab) """
 
         self.server_url = "http://10.27.193.245:3080"
         self.connector = Gns3Connector(url=self.server_url)
-        print(tabulate(self.connector.projects_summary(is_print=False), headers=["Project Name"]))
-        self.name = input("Input lab name: ")
-        self.lab = Project(name=self.name, connector=self.connector )
-        self.lab_get = self.lab.get()
+        self.list_labs = (tabulate(self.connector.projects_summary(is_print=False), headers=["Project Name"]))
+        print(self.list_labs)
+        self.name_lab = input("Input lab name: ")
         
 
     def all_proj (self):
@@ -30,28 +31,31 @@ class Base_gns():
 
         gns_ver = self.connector.get_version()
         return f"GNS3 ver is {gns_ver}"
-
+        
     def get_lab(self):
 
         """ Вернет lab_id, статус моей лабы """
 
-        print(self.lab.project_id)
-        return f"GNS3 lab_name: {self.lab.name}, lab_id:{self.lab.project_id}, lab status: {self.lab.status}"
+        lab = Project(name=self.name_lab , connector=self.connector )
+        print(lab.project_id)
+        return f"GNS3 lab_name: {lab.name}, lab_id:{lab.project_id}, lab status: {self.lab.status}"
     
     def get_nodes(self):
 
         """ Вернет все узлы в лабе"""
 
-        self.lab.open() # open lab
-        print(f"*** ALL nodes in {gns.lab.name} lab ***")
-        return  self.lab.nodes_summary()
+        lab = Project(name=self.name_lab , connector=self.connector )
+        lab.open() # open lab
+        
+        print(f"*** ALL nodes in {lab.name} lab ***")
+        return  lab.nodes_summary()
     
     def start_node(self):
 
         """ Запуск 1 узла в проекте"""
-
+        lab = Project(name=self.name_lab , connector=self.connector )
         r1 = Node(
-            project_id=self.lab.project_id, 
+            project_id=lab.project_id, 
             name='R1',
             connector=self.connector
             ) # создаем экз-р устр-ва
@@ -60,14 +64,17 @@ class Base_gns():
         stts_ret=r1.start()
         return stts_ret
 
-    def start_all_nodes(self):
+    def start_nodes_from_project(self):
 
-        """ pass """
-        print(self.lab)
-        print(self.lab_get)
-        self.lab.open()
-        print(self.lab.status)
-        result_starta=self.lab.start_nodes(poll_wait_time=5)
+        lab = Project(name=self.name_lab , connector=self.connector )
+        lab_get = lab.get()
+
+        print(lab)
+        print(lab_get)
+        lab.open()
+        print(lab.status)
+        result_starta=lab.start_nodes(poll_wait_time=5)
+        return lab.status
 
 if __name__=="__main__":
     gns= Base_gns()
@@ -76,4 +83,4 @@ if __name__=="__main__":
     # print(gns.get_lab(),'\n')
     # print( gns.get_nodes(),'\n')
     # print(gns.start_node())
-    print(gns.start_all_nodes())
+    print(gns.start_nodes_from_project())
