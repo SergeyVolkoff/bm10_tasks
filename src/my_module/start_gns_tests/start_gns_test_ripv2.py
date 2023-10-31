3910b51e36f6559749111f02c6be760562f38cb2
SergeyVolkoff <vol7745@yandex.ru>
2023-10-31 05:57:44 -0400

    bm

--- src/my_module/start_gns_tests/start_gns_test_ripv2.py
+++ src/my_module/start_gns_tests/start_gns_test_ripv2.py
@@ -9,7 +9,7 @@ sys.path.insert(1, os.path.join(sys.path[0],'..'))  # !!! PATH fo import with po
 # sys.path.insert(1, os.path.join(sys.path[0],'../command_cfg/'))  # !!! PATH fo import with position 1!!!
 # sys.path.append(os.path.join(os.getcwd(),'..'))     # !!! PATH fo import!!!
 
-# pprint.pprint(sys.path)
+pprint.pprint(sys.path)
 
 from ping3 import ping, verbose_ping
 from cfg_bm10 import Cfg_bm10
@@ -17,15 +17,17 @@ from base_gns3 import Base_gns
 from base_bm10 import Base_bm10
 
 
-# current_lab = Base_gns()
+# current_lab = Base_gns() # test wait this lab - SSV_auto_BM10_RIPv2
 # print(current_lab.start_nodes_from_project())
 
-with open("command_cfg/value_bm10.yaml")as f:
+with open("../command_cfg/value_bm10.yaml")as f:
     temp = yaml.safe_load(f)
     for t in temp:
         device = dict(t)
         r1 = Cfg_bm10(**device)
-        print(r1.cfg_ripv2(device,r1.commands_cfg_ripv2))
+        with open("../command_cfg/commands_cfg_ripv2.yaml") as f15:                # команды настройки Ripv2
+                commands = yaml.safe_load(f15)
+        print(r1.cfg_ripv2(device,commands))
 
         time.sleep(5)
         result=ping('192.168.1.1')
