4f95d749e5849c582c1055a04bf4fd170870bf73
SergeyVolkoff <vol7745@yandex.ru>
2023-11-12 12:27:38 -0500

    ya

--- src/my_module/start_gns_tests/start_gns_test_mwan3.py
+++ src/my_module/start_gns_tests/start_gns_test_mwan3.py
@@ -39,27 +39,37 @@ current_lab = Base_gns() # test wait this lab - SSV_auto_BM10_MWAN
 console.print("Стартует настройка лабы в gns3",style='info')
 time.sleep(5)
 print(current_lab.start_nodes_from_project())
-console.print("Стартует настройка DUT под тест mwan3\n" ,style='info')
+console.print("Стартует сброс DUT перед настройкой под тест mwan3\n" ,style='info')
 time.sleep(5)
 with open("../command_cfg/value_bm10.yaml")as f:
     temp = yaml.safe_load(f)
     for t in temp:
         device = dict(t)
         r1 = Cfg_bm10(**device)
-        with open("../command_cfg/commands_cfg_mwan3.yaml") as f15:                # команды настройки mwan3
-                command_cfg_mwan = yaml.safe_load(f15)
-        print(r1.cfg_mwan3(device,command_cfg_mwan))
-
+        with open("../command_cfg/commands_reset_cfg.yaml") as f14:                # команды настройки mwan3
+                commands_reset_cfg = yaml.safe_load(f14)
+        print(r1.cfg_mwan3(device,commands_reset_cfg))  # Сброс конфига
+        console.print("Стартует настройка DUT под тест mwan3\n" ,style='info')
         time.sleep(5)
-        result=ping('192.168.1.1')
-        while result is None:
-            result=ping('192.168.1.1')
-            print("DUT is rebooting, wait")
-            time.sleep(5)
-        else:
-            print("DUT up after reboot, wait all protocols!")
-            time.sleep(25)
-            print( "All up!")
+        with open("../command_cfg/value_bm10.yaml")as f:
+                temp = yaml.safe_load(f)
+                for t in temp:
+                    device = dict(t)
+                    r1 = Cfg_bm10(**device)
+                    with open("../command_cfg/commands_cfg_mwan3.yaml") as f15:                # команды настройки mwan3
+                            command_cfg_mwan = yaml.safe_load(f15)
+                    print(r1.cfg_mwan3(device,command_cfg_mwan))    # Настройка DUT
+
+        # time.sleep(5)
+        # result=ping('192.168.1.1')
+        # while result is None:
+        #     result=ping('192.168.1.1')
+        #     print("DUT is rebooting, wait")
+        #     time.sleep(5)
+        # else:
+        #     print("DUT up after reboot, wait all protocols!")
+        #     time.sleep(25)
+        #     print( "All up!")
 console.print("Стартует настройка tests под тест mwan3\n" ,style='info')
 time.sleep(5)
-pytest.main(["-v","-s","../tests_all/test_check_mwan3.py"])
+# pytest.main(["-v","-s","../tests_all/test_check_mwan3.py"])
