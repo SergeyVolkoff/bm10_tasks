791174673ea2bc20a0fe81d293a56e632c87aa7f
SergeyVolkoff <vol7745@yandex.ru>
2023-11-13 08:38:32 -0500

    bm_lepr

---
+++ src/my_module/start_cfg_dut/start_cfg_dmz.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import yaml
+sys.path.insert(1, os.path.join(sys.path[0],'..'))
+from cfg_bm10 import Cfg_bm10
+
+if __name__ == "__main__":
+    with open("../command_cfg/value_bm10.yaml")as f:
+        temp = yaml.safe_load(f)
+        for t in temp:
+            device = dict(t)
+            r1 = Cfg_bm10(**device)
+            with open("../command_cfg/commands_dmz_cfg.yaml") as f:                 # команды настройки 3G
+                commands_cfg_dmz = yaml.safe_load(f)
+            print(r1.cfg_vlan(device,commands_cfg_dmz)) 
