17cedffd6d4d6f252a8f56df2278363eb9cb7639
SergeyVolkoff <vol7745@yandex.ru>
2023-11-17 05:52:49 -0500

    bm_lepr

---
+++ src/my_module/start_cfg_dut/start_cfg_wan_lte.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import yaml
+sys.path.insert(1, os.path.join(sys.path[0],'..'))
+from cfg_bm10 import Cfg_bm10
+
+if __name__ == "__main__":
+    with open("../command_cfg/value_bm10.yaml")as f:
+        temp = yaml.safe_load(f)
+        for t in temp:
+            device = dict(t)
+            r1 = Cfg_bm10(**device)
+            with open("../command_cfg/commands_cfg_wan_lte.yaml") as f15:  # команды настройки mwan3
+                commands_cfg_wan_lte = yaml.safe_load(f15)
+            print(r1.cfg_ripv2(device,commands_cfg_wan_lte))
+
