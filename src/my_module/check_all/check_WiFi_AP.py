28ba6cfc51a92a55c41851eb97f48f5753c46973
SergeyVolkoff <vol7745@yandex.ru>
2023-11-22 14:03:06 -0500

    bm

--- src/my_module/check_all/check_WiFi_AP.py
+++ src/my_module/check_all/check_WiFi_AP.py
@@ -1,54 +1,76 @@
+import pprint
 import re
+import sys
+import os
+import time
+from py import test
 import yaml
-import netmiko
-from netmiko import (
-    ConnectHandler,
-    NetmikoTimeoutException,
-    NetmikoAuthenticationException,
-)
+
+
+sys.path.insert(1, os.path.join(sys.path[0],'..'))  # !!! PATH fo import with position 1!!!
+# pprint.pprint(sys.path)
+
+from ping3 import ping
+from base_gns3 import Base_gns
 from base_bm10 import Base_bm10
+from gns3fy import *
+
+from rich import print
+from rich.theme import Theme
+from rich.console import Console
+my_colors = Theme( #добавляет цветовую градацию для rich
+    {
+        "success":"bold green",
+        "fail":"bold red",
+        "warning":"bold yellow"
+    }
+)
+console = Console(theme=my_colors)
 
 
 '''
 Проверяем работу wifi моста поднятого с телефоном или роутером
 '''
 
-with open("/home/ssw/Documents/bm10_tasks/src/my_module/command_cfg/value_bm10.yaml") as f:
+with open("../command_cfg/value_bm10.yaml") as f:
     temp = yaml.safe_load(f)
     for t in temp:
         device = dict(t)
-        r1 = Router(**device)
-def check_WiFi_AP(comm):        # Определяем наличие настроенного бриджа (есть ли конфиг вообще нужный интерфейс)
+        r1 = Base_bm10(**device)
+
+def check_WiFi_AP(comm): 
+    # Определяем наличие настроенного бриджа (есть ли конфиг вообще нужный интерфейс)
     try:
-        temp = r1.send_sh_command(device, comm)
+        temp = r1.send_command(device, comm)
         if "Br_AP" in temp:
             return True
         else:
             return False
     except ValueError as err:
         return False
+    
 def check_ping_inet():
-    r1.ip_for_ping = "8.8.8.8"
-
+    ip_for_ping = "8.8.8.8"
     try:
-        res_ping_inet = r1.ping_ip(device,r1.command_ping)
+        res_ping_inet = r1.ping_inet(device)
         if "destination available" in res_ping_inet:
             print("Bridge OK")
         else:
             print("Bridge bad, inet(8.8.8.8)- not available")
     except ValueError as err:
         return False
-def check_pingGW():     # Пингуем шлюз-телефон
-    output_rout = r1.send_sh_command(device,"ip route")              # этой командой получаем основной маршрут
+    
+def check_pingGW(): 
+     # Пингуем шлюз-телефон
+    output_rout = r1.send_command(device,"ip route")              # этой командой получаем основной маршрут
+    print(output_rout)
     ip_route = re.search(r'default via (\S+)',output_rout).group(1)  # реджектим ip шлюза
     r1.ip_for_ping=ip_route                                          # переопределяем ip из основного класса R1
-
     try:
         res_pingGW = r1.ping_ip(device,r1.command_ping)           # проверяем доступность шлюза (
         if "destination  available " in res_pingGW:               #если отвечает, значит все ок, возвращаем тру
             print("GW available!")
             return True
-
         else:
             if " out of destination" in res_pingGW:            #если не отвечает - не правльно настроено, возвращаем фолс
                 print("GW out")
@@ -59,5 +81,6 @@ def check_pingGW():     # Пингуем шлюз-телефон
 
 
 if __name__ =="__main__":
-    result = check_ping_inet()
+    comm = 'uci show network.Br_AP'
+    result = check_pingGW()
     print (result)
